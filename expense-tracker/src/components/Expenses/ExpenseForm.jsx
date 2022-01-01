36e6d91ba3c39ef91dc9c6576c1c253e71e64df2
arfaoui-aziz <mohamedaziz.arfaoui@esprit.tn>
2022-01-01 17:01:31 +0100

    feat: handle ExpenseForm Inputs

--- expense-tracker/src/components/Expenses/ExpenseForm.jsx
+++ expense-tracker/src/components/Expenses/ExpenseForm.jsx
@@ -1,20 +1,51 @@
+import { useState } from "react";
 import "../../assets/css/ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [expenseData, setExpenseData] = useState({
+    title: "",
+    amount: 0.01,
+    date: new Date().toISOString().split("T")[0],
+  });
+
+  const changeHandler = (e) => {
+    const input = e.target;
+    setExpenseData({ ...expenseData, [input.name]: input.value });
+  };
+
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label htmlFor="title">Title</label>
-          <input type="text" name="title" />
+          <input
+            type="text"
+            name="title"
+            onChange={changeHandler}
+            value={expenseData.title}
+          />
         </div>
         <div className="new-expense__control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" name="amount" min="0.01" step="0.01" />
+          <input
+            type="number"
+            name="amount"
+            min="0.01"
+            step="0.01"
+            onChange={changeHandler}
+            value={expenseData.amount}
+          />
         </div>
         <div className="new-expense__control">
           <label htmlFor="date">Date</label>
-          <input type="date" name="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            type="date"
+            name="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={expenseData.date}
+            onChange={changeHandler}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
