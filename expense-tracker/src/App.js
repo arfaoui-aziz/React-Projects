fa44761848bd6387859d69c58cfb1d02cf1f0586
arfaoui-aziz <mohamedaziz.arfaoui@esprit.tn>
2022-01-01 16:36:55 +0100

    feat: add AddExpense + ExpenseForm Components

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,10 +1,12 @@
 import expenses from "./data/expenses";
 import Expenses from "./components/Expenses/Expenses";
+import AddExpense from "./components/Expenses/AddExpense";
 
 function App() {
   return (
     <div>
       <h2 className="u-centerText">Let's get started!</h2>
+      <AddExpense />
       <Expenses items={expenses} />
     </div>
   );
