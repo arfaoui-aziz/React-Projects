5eba789f9851b2009d3d4a06b51f09d4a30fe12d
arfaoui-aziz <mohamedaziz.arfaoui@esprit.tn>
2022-01-01 16:18:56 +0100

    feat: Update ExpenseItem Title

--- expense-tracker/src/components/Expenses/ExpenseItem.jsx
+++ expense-tracker/src/components/Expenses/ExpenseItem.jsx
@@ -1,19 +1,24 @@
+import { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../Card";
 import "../../assets/css/ExpenseItem.css";
 
 export default function ExpenseItem(props) {
-  const clickHandler = (e) => {};
+  const [title, setTitle] = useState(props.title);
+  const clickHandler = (e) => {
+    setTitle("Updated !");
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">$ {props.amount}</div>
       </div>
-      <button onClick={clickHandler }>Change Title</button>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
