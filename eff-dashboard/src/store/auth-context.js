518e9bd0c412245672ecc26997fda73d6cad83c1
arfaoui-aziz <mohamedaziz.arfaoui@esprit.tn>
2022-01-09 12:06:48 +0100

    eff-dashboard: manage AuthContexte

--- eff-dashboard/src/store/auth-context.js
+++ eff-dashboard/src/store/auth-context.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const AuthConetxt = React.createContext({
+const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: () => {},
 });
 
-export default AuthConetxt;
+export default AuthContext;
+
+export const AuthContextProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedUserLoggedInInformation = localStorage.getItem("isLoggedIn");
+
+    if (storedUserLoggedInInformation === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn,
+        onLogin: loginHandler,
+        onLogout: logoutHandler,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
