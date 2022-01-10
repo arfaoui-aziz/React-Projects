4e31560feee17f98d5a3046b113ede1c74110511
arfaoui-aziz <mohamedaziz.arfaoui@esprit.tn>
2022-01-10 18:06:44 +0100

    feat: foodify add header banner

--- foodify/src/components/Header/Header.jsx
+++ foodify/src/components/Header/Header.jsx
@@ -1,17 +1,23 @@
 import Button from "../UI/Button";
 import Container from "../UI/Container";
 import styles from "./Header.module.css";
+import MainImg from "../../assets/img/banner2.jpg";
 
 const Header = () => {
   return (
-    <header className={styles.header}>
-      <Container className={styles.header__nav}>
-        <h1 className={styles.header__logo}>Foodify</h1>
-        <Button type="button" className={styles.header__button}>
-          Your Cart <span className={styles.header__itemsCount}>0</span>{" "}
-        </Button>
-      </Container>
-    </header>
+    <>
+      <header className={styles.header}>
+        <Container className={styles.header__nav}>
+          <h1 className={styles.header__logo}>Foodify</h1>
+          <Button type="button" className={styles.header__button}>
+            Your Cart <span className={styles.header__itemsCount}>0</span>{" "}
+          </Button>
+        </Container>
+      </header>
+      <div className={styles["main-image"]}>
+        <img src={MainImg} alt="Foodify Main " />
+      </div>
+    </>
   );
 };
 
